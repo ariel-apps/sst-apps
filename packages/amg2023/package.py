b8c752854f6cc47fa55ad49e691541d9be69304f
Patrick Lavin <plavin997@gmail.com>
2025-02-24 17:14:54 -0700

    update amg2023 to work with libarielapi

--- packages/amg2023/package.py
+++ packages/amg2023/package.py
@@ -1,12 +1,14 @@
 from spack.package import *
 from spack.pkg.builtin.amg2023 import Amg2023 as BuiltinAmg2023
+from spack.util.environment import EnvironmentModifications
+from pathlib import Path
 
 class Amg2023(BuiltinAmg2023):
 
     git = "https://github.com/ariel-apps/AMG2023.git"
     variant("ariel", default=True, description="Enable Ariel API integration")
 
-    depends_on("sst-elements")
+    depends_on("sst-elements@develop")
 
     depends_on(
         "sst-elements+ariel_mpi",
@@ -16,4 +18,11 @@ class Amg2023(BuiltinAmg2023):
     def cmake_args(self):
         cmake_options = super().cmake_args()
         cmake_options.append(self.define_from_variant("AMG_WITH_ARIELAPI", "ariel"))
+
+        env_mod = EnvironmentModifications()
+        env_mod.set('SST_ELEMENTS_INSTALL', self.spec['sst-elements'].prefix)
+        sst_elements_lib = Path(self.spec['sst-elements'].prefix) / 'lib' / 'sst-elements-library'
+        env_mod.apply_modifications()
+
+        cmake_options.append("-DCMAKE_EXE_LINKER_FLAGS=-L{0}".format(sst_elements_lib))
         return cmake_options
